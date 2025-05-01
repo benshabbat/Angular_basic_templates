da464d6ad3e7a0e3a18657a53842ab7ef3a4da16
benshabbat <benshabbat27@gmail.com>
2025-05-01 21:51:51 +0300

    feat: update app structure by adding router outlet and removing home component reference

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
-import { HomeComponent } from "./home/home.component";
 import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ HeaderComponent, HomeComponent, FooterComponent],
+  imports: [ RouterOutlet,HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

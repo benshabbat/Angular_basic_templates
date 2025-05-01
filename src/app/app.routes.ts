fb3f4e775f718b91ce3b3ef6709aca2475a59223
benshabbat <benshabbat27@gmail.com>
2025-05-01 21:39:28 +0300

    feat: add About component with routing and basic structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { UsersComponent } from './users/users.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'users',
+    component: UsersComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
